e64699e20ffc3d87c9f79a1ccfdc7e1c86952a05
Peter Kalmykov <3dmodels@ukr.net>
2019-09-03 02:04:55 +0300

    второй коммит, задание 009

--- script.js
+++ script.js
@@ -1,15 +1,77 @@
-var money = prompt("Ваш бюджет на месяц?",'');
+var money = +prompt("Ваш бюджет на месяц?",'');
 var time = prompt("Введите дату в формате YYYY-MM-DD",'');
-var CompulsoryCosts = prompt("Введите обязательную статью расходов в этом месяце",''); 
-var CompulsoryCostsPrice = prompt("Во сколько обойдется?");
 
-var AppData = {
+var аppData = {
     budget: money,
     timeData: time,
-    expenses:{CompulsoryCosts:CompulsoryCostsPrice},
+    expenses:{},
     optionalExpenses:{},
     income:[],
     savings: false
 };
 
-alert (money/30);
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце",''), 
+//         b = +prompt("Во сколько обойдется?",'');
+
+//     if ( (typeof(a))==='string' && (typeof(a)) != null 
+//         && (typeof(b)) != null && a != '' && b != ''
+//         && a.length < 50) {
+//         console.log ("done");
+//         аppData.expenses[a] = b;
+//     } else {
+//         alert ("Вы не ввели значение, попробуйте ещё раз");
+//         i--;
+//         console.log ("i - 1");
+//     }
+//     i++;
+// } while (i < 2);
+
+// let i = 0;
+// while (i < 2) {
+//         let a = prompt("Введите обязательную статью расходов в этом месяце",''), 
+//         b = +prompt("Во сколько обойдется?",'');
+
+//     if ( (typeof(a))==='string' && (typeof(a)) != null 
+//         && (typeof(b)) != null && a != '' && b != ''
+//         && a.length < 50) {
+//         console.log ("done");
+//         аppData.expenses[a] = b;
+//     } else {
+//         alert ("Вы не ввели значение, попробуйте ещё раз");
+//         i--;
+//         console.log ("i - 1");
+//     }
+//     i++;
+// }
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''), 
+        b = +prompt("Во сколько обойдется?",'');
+
+    if ( (typeof(a))==='string' && (typeof(a)) != null 
+        && (typeof(b)) != null && a != '' && b != ''
+        && a.length < 50) {
+        console.log ("done");
+        аppData.expenses[a] = b;
+    } else {
+        alert ("Вы не ввели значение, попробуйте ещё раз");
+        i--;
+        console.log ("i - 1");
+    }
+};
+
+аppData.moneyPerDay = аppData.budget/30;
+alert ("Ежедневный бюджет: " + аppData.moneyPerDay);
+
+if (аppData.moneyPerDay < 100) {
+    console.log ("Минимальный уровень достатка");
+} else if (аppData.moneyPerDay > 100 && аppData.moneyPerDay < 2000){
+    console.log ("Средний уровень достатка");
+} else if (аppData.moneyPerDay > 2000 ) {
+    console.log ("Средний уровень достатка");
+} else {
+    console.log ("Произошла ошибка");
+}
